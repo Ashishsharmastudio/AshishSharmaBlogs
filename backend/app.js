33844d437ec79116cee5f7a5bd590760cc9fde76
Ashish <Ashishsharmastudio@gmail.com>
2024-04-08 23:02:22 -0700

    changes in app

--- backend/app.js
+++ backend/app.js
@@ -33,6 +33,13 @@ app.use(
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/blog", blogRouter);
 
+app.get("/", (req,res,next) => {
+  return res.status(200).json({
+    success: true,
+    message: "Hello World!"
+  });
+});
+
 dbConnection();
 app.use(errorMiddleware);
 
