a4e9abb3692a13f7705b0a3c3662542244721dc0
Ashish <Ashishsharmastudio@gmail.com>
2024-04-28 09:34:35 +0530

    changes user comtroler

--- frontend/src/components/pages/AllAuthors.jsx
+++ frontend/src/components/pages/AllAuthors.jsx
@@ -6,26 +6,41 @@ import axios from "axios";
 const AllAuthors = () => {
   const { mode } = useContext(Context);
   const [authors, setAuthors] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const featchAuthors = async () => {
-      const { data } = await axios.get(
-        "https://ashishsharmablogs.onrender.com/api/v1/user/authors",
-        { withCredentials: true }
-      );
-      setAuthors(data.authors);
+      setLoading(true);
+      try {
+        const { data } = await axios.get(
+          "https://ashishsharmablogs.onrender.com/api/v1/user/authors",
+          { withCredentials: true }
+        );
+        setAuthors(data.authors);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
     featchAuthors();
   }, []);
+
   return (
     <article
       className={
-        mode === "dark" ? "dark-bg all-authors" : "light-bg all-authors"
+        mode === "dark"? "dark-bg all-authors" : "light-bg all-authors"
       }
     >
       <h2>ALL AUTHORS</h2>
       <div className="container">
-        {authors && authors.length > 0 ? (
+        {loading? (
+          <BeatLoader size={50} color="gray" style={{ padding: "200px 0" }} />
+        ) : error? (
+          <p style={{ color: "red" }}>{error}</p>
+        ) : (
           authors.map((element) => {
             return (
               <div className="card" key={element._id}>
@@ -35,8 +50,6 @@ const AllAuthors = () => {
               </div>
             );
           })
-        ) : (
-          <BeatLoader size={50} color="gray" style={{ padding: "200px 0" }} />
         )}
       </div>
     </article>
